505d4cb3b3b80f2d99b55763662248244d9b4431
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-07-31 12:16:12 -0400

    Nicer error

--- setup/clone.ts
+++ setup/clone.ts
@@ -1,11 +1,13 @@
 import { streams } from "./deps.ts";
 
 export default async function cloneTemplateToCache(path: string, ref: string): Promise<void> {
-    const process = Deno.run({
-        "cmd": ["git", "clone", "--depth", "1", `-o`, ref, `https://github.com/Swifter1243/ReMapper-Setup.git`, path],
-        stdout: "piped",
-        stderr: "piped",
-    })
+    try {
+        const process = Deno.run({
+            "cmd": ["git", "clone", "--depth", "1", `-o`, ref, `https://github.com/Swifter1243/ReMapper-Setup.git`, path],
+            stdout: "piped",
+            stderr: "piped",
+        })
+
 
     streams.copy(process.stdout, Deno.stdout);
     streams.copy(process.stderr, Deno.stderr);
@@ -15,6 +17,16 @@ export default async function cloneTemplateToCache(path: string, ref: string): P
         console.error("Failed to git clone")
         console.error(`Received code: ${status.code}`)
         Deno.exit(status.code)
+        }
+    } catch (e) {
+        if (e instanceof Deno.errors.NotFound) {
+
+            if (Deno.build.os === "linux") {
+                console.error("Download git using your system's package manager")
+            } else {
+                console.error("Download git using at https://git-scm.com/downloads")
+            }
+        }
     }
 
     console.log(`Cloned the template with revision ${ref} to ${path}`)
