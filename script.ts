c984207a20ff878c68f81600ee7be53427f0f5aa
Swifter <61858676+Swifter1243@users.noreply.github.com>
2023-07-05 12:19:59 -0400

    Update to 3.1.2

--- script.ts
+++ script.ts
@@ -1,4 +1,4 @@
-import { Difficulty } from "https://deno.land/x/remapper@3.1.1/src/mod.ts"
+import { Difficulty } from "https://deno.land/x/remapper@3.1.2/src/mod.ts"
 
 const map = new Difficulty("ExpertPlusLawless", "ExpertPlusStandard");
 
