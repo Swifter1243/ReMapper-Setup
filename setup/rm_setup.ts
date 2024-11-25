fdb21fe0dea7efe0910d6d36dc36ea84809be4ba
Swifter <swifter1243@gmail.com>
2024-11-25 14:11:15 -0500

    some more organization

--- setup/rm_setup.ts
+++ setup/rm_setup.ts
@@ -3,26 +3,16 @@
 import cloneTemplateToCache from "./clone.ts";
 import { configDir, path, fs } from "./deps.ts";
 
-const name = Deno.args[0] ?? "."
-const includeMapFiles = Deno.args.some(e => e === "--map" || e === "-m") || Deno.args[0]
+function getCacheBaseDirectory() {
+    const userDir = configDir();
 
-const refIndex = Deno.args.findIndex(e => e === "--version" || e === "-v")
-let ref = refIndex !== -1 ? Deno.args[refIndex + 1] : undefined
+    if (!userDir) {
+        console.error("Unable to find user config directory!");
+        Deno.exit(1);
+    }
 
-const currentFolder = Deno.cwd();
-
-const destFolder = path.join(currentFolder, name)
-
-if (name !== ".") await Deno.mkdir(destFolder)
-
-const userDir = configDir();
-
-if (!userDir) {
-    console.error("Unable to find user config directory!");
-    Deno.exit(1);
+    return path.join(userDir, "remapper_setup")
 }
-
-const remapperDirectory = path.join(userDir, "remapper_setup")
 
 async function getLatestReleaseTag(uri: string) {
     const latestRelease = await fetch(uri, {
@@ -38,51 +28,72 @@ async function getLatestReleaseTag(uri: string) {
     return json["tag_name"]!
 }
 
-if (!ref) {
-    ref = await getLatestReleaseTag("https://api.github.com/repos/Swifter1243/ReMapper-Setup/releases/latest")
+async function getCacheVersionPath(cacheBaseDirectory: string, version: string) {
+    const templatePath = path.join(cacheBaseDirectory, version)
+
+    try {
+        await Deno.stat(templatePath)
+    } catch (e) {
+        if (e instanceof Deno.errors.NotFound) {
+            await cloneTemplateToCache(templatePath, version);
+        } else {
+            throw e;
+        }
+    }
+
+    // Validate the path exists
+    await Deno.stat(templatePath)
+
+    return templatePath
 }
 
-if (!ref) {
-    console.error(`No ref specified`)
-    Deno.exit(2)
-}
+const name = Deno.args[0] ?? "."
+const includeMapFiles = Deno.args.some(e => e === "--map" || e === "-m") || Deno.args[0]
 
+const versionIndex = Deno.args.findIndex(e => e === "--version" || e === "-v")
+let version = versionIndex !== -1 ? Deno.args[versionIndex + 1] : undefined
+
+const currentFolder = Deno.cwd();
+
+const destFolder = path.join(currentFolder, name)
+
+if (name !== ".") await Deno.mkdir(destFolder)
+
+const cacheBaseDirectory = getCacheBaseDirectory()
+
+// make sure that cache base directory exists
 try {
-    await Deno.mkdir(remapperDirectory, {
+    await Deno.mkdir(cacheBaseDirectory, {
         recursive: true
     })
 } catch (e) {
     if (e! instanceof Deno.errors.AlreadyExists) throw e;
 }
 
-const templatePath = path.join(remapperDirectory, ref)
-
-try {
-    await Deno.stat(templatePath)
-} catch (e) {
-    if (e instanceof Deno.errors.NotFound) {
-        await cloneTemplateToCache(templatePath, ref);
-    } else {
-        throw e;
-    }
+if (!version) {
+    version = await getLatestReleaseTag("https://api.github.com/repos/Swifter1243/ReMapper-Setup/releases/latest")
 }
 
-// Validate the path exists
-await Deno.stat(templatePath)
+if (!version) {
+    console.error(`No ref specified`)
+    Deno.exit(2)
+}
+
+const cacheVersionPath = await getCacheVersionPath(cacheBaseDirectory, version)
 
 // now copy to path
 const ignoredFiles = ["setup", ".git"]
 
 const tasks: Promise<void>[] = []
 
-console.log("Copying from template path", templatePath)
-for await (const file of Deno.readDir(templatePath)) {
+console.log("Copying from template path", cacheVersionPath)
+for await (const file of Deno.readDir(cacheVersionPath)) {
     if (ignoredFiles.includes(file.name)) continue
 
     // Ignore .dat files if need be
     if (!includeMapFiles && path.extname(file.name) == ".dat") continue;
 
-    const src = path.join(templatePath, file.name)
+    const src = path.join(cacheVersionPath, file.name)
     const dest = path.join(destFolder, file.name)
 
     tasks.push(fs.copy(src, dest))
